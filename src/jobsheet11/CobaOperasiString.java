8d9b9d50be4961fd4ec51427148ba67d0f81b38e
WIN 10 <WIN 10@DESKTOP-MN29TUB>
2018-08-13 14:01:10 +0700

    Coba method length & isEmpty

--- src/jobsheet11/CobaOperasiString.java
+++ src/jobsheet11/CobaOperasiString.java
@@ -5,6 +5,18 @@ public class CobaOperasiString {
     {
         String identitas = "Zidan Kamal Arzin / X RPL 2 / 40";
         System.out.println("Identitas : " + identitas);
+    
+    
+        String x = "Operasi";
+        System.out.println("isi variable x : " + x);
+        System.out.println("\"" + x + "\" panjang karakter = " + x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variable y : " + y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
+        
     }
     
 }
