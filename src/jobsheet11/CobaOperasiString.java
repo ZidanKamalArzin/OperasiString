1e3cf2c6666e294863f438dc611bd40d1edb48dd
WIN 10 <WIN 10@DESKTOP-MN29TUB>
2018-08-13 13:44:21 +0700

    Initial Commit

---
+++ src/jobsheet11/CobaOperasiString.java
@@ -0,0 +1,10 @@
+package jobsheet11;
+import java.util.Scanner;
+public class CobaOperasiString {
+    public static void main(String[] args)
+    {
+        String identitas = "Zidan Kamal Arzin / X RPL 2 / 40";
+        System.out.println("Identitas : " + identitas);
+    }
+    
+}
