110752154542b3eadabdf9e647a6e4c5a897d6ee
WIN 10 <WIN 10@DESKTOP-MN29TUB>
2018-08-14 21:02:49 +0700

    Initial Commit

---
+++ src/jobsheet11/ConvertVokal2Angka.java
@@ -0,0 +1,13 @@
+
+package jobsheet11;
+
+public class ConvertVokal2Angka {
+private static void tampilJudul(String identitas) 
+    {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka\n");
+    }
+
+}
+
