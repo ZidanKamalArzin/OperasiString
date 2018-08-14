1239f650f20c2c7a337f20c62aaf35f02f564ab4
WIN 10 <WIN 10@DESKTOP-MN29TUB>
2018-08-14 21:07:04 +0700

    Tambah input

--- src/jobsheet11/ConvertVokal2Angka.java
+++ src/jobsheet11/ConvertVokal2Angka.java
@@ -1,12 +1,21 @@
 
 package jobsheet11;
-
+import java.util.Scanner;
 public class ConvertVokal2Angka {
 private static void tampilJudul(String identitas) 
     {
         System.out.println("Identitas : "+ identitas);
         
         System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka\n");
+    }
+private static String tampilInput() {
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("Masukan Kalimat : ");
+    String kalimat = scanner.nextLine();
+    System.out.println("Kalimat Asli : " + kalimat );
+    
+    return kalimat;
     }
 
 }
