15b43a0e42984cb76e7a1bb8622b1acdc8d025db
WIN 10 <WIN 10@DESKTOP-MN29TUB>
2018-08-16 18:38:50 +0700

    Initial Commit

--- src/jobsheet11/CobaOperasiString.java
+++ src/jobsheet11/CobaOperasiString.java
@@ -1,22 +1,11 @@
+
 package jobsheet11;
 import java.util.Scanner;
 public class CobaOperasiString {
     public static void main(String[] args)
     {
-        String identitas = "Zidan Kamal Arzin / X RPL 2 / 40";
-        System.out.println("Identitas : " + identitas);
-    
-    
-        String x = "Operasi";
-        System.out.println("isi variable x : " + x);
-        System.out.println("\"" + x + "\" panjang karakter = " + x.length());
-        
-        System.out.println("x adalah kosong : " + x.isEmpty());
-        
-        String y = "";
-        System.out.println("isi variable y : " + y);
-        System.out.println("y adalah kosong : " + y.isEmpty());
-        
+          String identitas = "Zidan Kamal Arzin / X RPL 2 / 40";
+        System.out.println("Identitas : "+ identitas);
     }
     
 }
