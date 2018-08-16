cfacf2e53775915701d719518c1890174441d1b7
WIN 10 <WIN 10@DESKTOP-MN29TUB>
2018-08-16 18:57:17 +0700

    Coba method compareTo & compareToIgnoreCase

--- src/jobsheet11/CobaOperasiString.java
+++ src/jobsheet11/CobaOperasiString.java
@@ -26,9 +26,24 @@ public class CobaOperasiString {
         
         String r = "operasi";
         System.out.println("isi cariable r : "+ r);
-        System.out.println("isi variable s sama dengan r (Case Sensitive): " + x.equals(r));
+        System.out.println("isi variable s sama dengan r (Case Sensitive): " 
+                + x.equals(r));
         System.out.println("isi x sama dengan r (Not Case Sensitive) : " 
                 + x.equalsIgnoreCase(r));
+        
+          System.out.println("perbandingan isi x dengan y : " 
+                  + x.compareTo(y));
+        System.out.println("perbandingan isi x dengan z (Case Sensitive): " 
+                + x.compareTo(z));
+         System.out.println("perbandingan isi x dengan r (Case Sensitive): " 
+                + x.compareTo(r));
+        System.out.println("perbandingan isi x dengan r (Not Case Sensitive): " 
+                + x.compareToIgnoreCase(r));
+        
+        String s = "operasI";
+        System.out.println("isi variable s : "+ s);
+        System.out.println("perbandingan isi r dengan s (Case Sensitive) : " 
+                + r.compareTo(s));
         
         
     }
